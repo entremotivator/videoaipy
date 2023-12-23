e4b58ddd5fdc65f856db9e2e75a37e2df01ac723
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-22 21:16:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,74 +1,63 @@
-import os
-
 import streamlit as st
+from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip, vfx
 
-# These are the formats supported in Streamlit right now.
-VIDEO_EXTENSIONS = ["mp4", "ogv", "m4v", "webm"]
+def edit_video(input_path, output_path, start_time, end_time, text, font_size=70, text_color='white', playback_speed=1.0, output_format="webm", text_position='center'):
+    try:
+        # Load video clip and extract subclip
+        video = VideoFileClip(input_path).subclip(start_time, end_time)
 
-# For sample video files, try the Internet Archive, or download a few samples here:
-# http://techslides.com/sample-webm-ogg-and-mp4-video-files-for-html5
+        # Make the text clip
+        txt_clip = (TextClip(text, fontsize=font_size, color=text_color, bg_color='transparent')
+                    .set_position(text_position)
+                    .with_duration(video.duration))
 
+        # Composite video with text
+        result = CompositeVideoClip([video, txt_clip])
 
-st.title("Video Widget Examples")
+        # Adjust playback speed
+        result = result.fx(vfx.speedx, playback_speed)
 
-st.header("Local video files")
-st.write(
-    "You can use st.video to play a locally-stored video by supplying it with a valid filesystem path."
-)
+        # Write the edited video to the output path
+        result.write_videofile(output_path, codec=output_format, audio_codec='aac', temp_audiofile='temp-audio.m4a', remove_temp=True)
 
+        return True
+    except Exception as e:
+        st.error(f"Error: {str(e)}")
+        return False
 
-def get_video_files_in_dir(directory):
-    out = []
-    for item in os.listdir(directory):
-        try:
-            name, ext = item.split(".")
-        except:
-            continue
-        if name and ext:
-            if ext in VIDEO_EXTENSIONS:
-                out.append(item)
-    return out
+def display_video(file_path, message=""):
+    if st.button("Play Video"):
+        st.video(file_path)
 
+def main():
+    st.title("Video Editor App")
 
-avdir = os.path.expanduser("~")
-files = get_video_files_in_dir(avdir)
+    uploaded_file = st.file_uploader("Upload a video file", type=["mp4"])
 
-if len(files) == 0:
-    st.write(
-        "Put some video files in your home directory (%s) to activate this player."
-        % avdir
-    )
+    if uploaded_file:
+        st.subheader("Preview Uploaded Video:")
+        display_video(uploaded_file)
 
-else:
-    filename = st.selectbox(
-        "Select a video file from your home directory (%s) to play" % avdir,
-        files,
-        0,
-    )
+        col1, col2 = st.beta_columns([2, 1])
+        with col1:
+            start_time, end_time = st.slider("Select time range (seconds)", 0.0, 60.0, (0.0, 10.0))
+            text = st.text_input("Enter text to overlay on the video", "My Holidays 2013")
+            font_size = st.slider("Select text font size", 1, 100, 70)
+            text_color = st.color_picker("Select text color", "#ffffff")
+            text_position = st.selectbox("Select text position", ["center", "top", "bottom", "left", "right"])
 
-    st.video(os.path.join(avdir, filename))
-st.header("Remote video playback")
-st.write("st.video allows a variety of HTML5 supported video links, including YouTube.")
+        with col2:
+            playback_speed = st.slider("Select video playback speed", 0.1, 3.0, 1.0, step=0.1)
+            output_format = st.selectbox("Select output format", ["webm", "mp4"])
 
+        if st.button("Edit Video"):
+            with st.spinner("Processing..."):
+                output_path = f"edited_video.{output_format}"
+                success = edit_video(uploaded_file, output_path, start_time[0], start_time[1], text, font_size, text_color, playback_speed, output_format, text_position)
 
-def shorten_vid_option(opt):
-    return opt.split("/")[-1]
+            if success:
+                st.success(f"Video edited successfully. [Download Edited Video](./{output_path})")
+                display_video(output_path, "Edited Video:")
 
-
-# A random sampling of videos found around the web.  We should replace
-# these with those sourced from the streamlit community if possible!
-vidurl = st.selectbox(
-    "Pick a video to play",
-    (
-        "https://youtu.be/_T8LGqJtuGc",
-        "https://www.youtube.com/watch?v=kmfC-i9WgH0",
-        "https://www.youtube.com/embed/sSn4e1lLVpA",
-        "http://www.rochikahn.com/video/videos/zapatillas.mp4",
-        "http://www.marmosetcare.com/video/in-the-wild/intro.webm",
-        "https://www.orthopedicone.com/u/home-vid-4.mp4",
-    ),
-    0,
-    shorten_vid_option,
-)
-
-st.video(vidurl)
+if __name__ == "__main__":
+    main()
