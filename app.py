92dcdd88542aae1bfac806efefc4fe30c1dbf1c9
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-22 21:20:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,39 @@
 import streamlit as st
-from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip, vfx
+from moviepy.editor import VideoFileClip
+import os
 
-def edit_video(input_path, output_path, start_time, end_time, text, font_size=70, text_color='white', playback_speed=1.0, output_format="webm", text_position='center'):
-    try:
-        # Load video clip and extract subclip
-        video = VideoFileClip(input_path).subclip(start_time, end_time)
+# Sidebar file uploader
+files = st.sidebar.file_uploader('File uploader', accept_multiple_files=True, type=['mp4'])
 
-        # Make the text clip
-        txt_clip = (TextClip(text, fontsize=font_size, color=text_color, bg_color='transparent')
-                    .set_position(text_position)
-                    .with_duration(video.duration))
+# Temporary directory to save uploaded videos
+temp_dir = 'temp_videos'
+os.makedirs(temp_dir, exist_ok=True)
 
-        # Composite video with text
-        result = CompositeVideoClip([video, txt_clip])
+# Function to process each uploaded video
+def process_video(file):
+    video_path = os.path.join(temp_dir, file.name)
+    
+    # Save the uploaded video to the temporary directory
+    with open(video_path, 'wb') as f:
+        f.write(file.read())
 
-        # Adjust playback speed
-        result = result.fx(vfx.speedx, playback_speed)
+    # Load the video using VideoFileClip
+    video_clip = VideoFileClip(video_path)
 
-        # Write the edited video to the output path
-        result.write_videofile(output_path, codec=output_format, audio_codec='aac', temp_audiofile='temp-audio.m4a', remove_temp=True)
+    # Process the video (add your custom processing logic here)
+    # For example, you can get the duration of the video
+    duration = video_clip.duration
 
-        return True
-    except Exception as e:
-        st.error(f"Error: {str(e)}")
-        st.exception(e)  # Display exception details for debugging
-        return False
+    # Return the result or do further processing
+    return duration
 
-def display_video(file_path, message=""):
-    if st.button("Play Video"):
-        st.video(file_path)
+# Process each uploaded file
+for file in files:
+    duration = process_video(file)
+    st.write(f"Video '{file.name}' duration: {duration} seconds")
 
-def main():
-    st.title("Video Editor App")
+# Cleanup: Remove temporary directory after processing
+st.experimental_rerun()
 
-    uploaded_file = st.file_uploader("Upload a video file", type=["mp4"])
-
-    if uploaded_file:
-        st.subheader("Preview Uploaded Video:")
-        display_video(uploaded_file)
-
-        col1, col2 = st.beta_columns([2, 1])
-        with col1:
-            start_time, end_time = st.slider("Select time range (seconds)", 0.0, 60.0, (0.0, 10.0))
-            text = st.text_input("Enter text to overlay on the video", "My Holidays 2013")
-            font_size = st.slider("Select text font size", 1, 100, 70)
-            text_color = st.color_picker("Select text color", "#ffffff")
-            text_position = st.selectbox("Select text position", ["center", "top", "bottom", "left", "right"])
-
-        with col2:
-            playback_speed = st.slider("Select video playback speed", 0.1, 3.0, 1.0, step=0.1)
-            output_format = st.selectbox("Select output format", ["webm", "mp4"])
-
-        if st.button("Edit Video"):
-            with st.spinner("Processing..."):
-                output_path = f"edited_video.{output_format}"
-                success = edit_video(uploaded_file.name, output_path, start_time[0], start_time[1], text, font_size, text_color, playback_speed, output_format, text_position)
-
-            if success:
-                st.success(f"Video edited successfully. [Download Edited Video](./{output_path})")
-                display_video(output_path, "Edited Video:")
-
-if __name__ == "__main__":
-    main()
+# Note: The cleanup logic may vary depending on your use case
+# In a production scenario, consider a more robust cleanup mechanism
